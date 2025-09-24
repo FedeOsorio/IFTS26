1b146d6beee0227925ceaa6865393c46475cc49e
FedeO <fede.osorio@outlook.com.ar>
2025-09-24 20:59:11 -0300

    Se crea estructura de carpetas y enrutamiento básico

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { NotFound } from './pages/not-found/not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/inicio', pathMatch: 'full' },
+    { path: 'inicio', component: Home },
+    { path: 'not-found', component: NotFound},
+    { path: '**', redirectTo: '/not-found', pathMatch: 'full' }
+];
