663484f60bf7d712f6b5a2060fd1d0a2c1fe5897
FedeO <fede.osorio@outlook.com.ar>
2025-11-18 13:23:01 -0300

    Componente pre-inscripcion

--- src/app/pages/navegacion/pre-inscripcion/pre-inscripcion.ts
+++ src/app/pages/navegacion/pre-inscripcion/pre-inscripcion.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser'; // Importante
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faCalendarCheck, faLocationDot } from '@fortawesome/free-solid-svg-icons';
+
+import dataPreInscripcion from '../../../../assets/data/pre-inscripcion.json';
 
 @Component({
   selector: 'app-pre-inscripcion',
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './pre-inscripcion.html',
   styleUrl: './pre-inscripcion.scss'
 })
 export class PreInscripcion {
+  private sanitizer = inject(DomSanitizer);
 
+  // Asignamos los datos
+  data = dataPreInscripcion;
+
+  // Íconos
+  iconoCalendario = faCalendarCheck;
+  iconoUbicacion = faLocationDot;
+
+  // Función para limpiar la URL del video/presentación
+  getSafeUrl(url: string): SafeResourceUrl {
+    return this.sanitizer.bypassSecurityTrustResourceUrl(url);
+  }
 }
